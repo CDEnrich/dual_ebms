b2f8b560beffdca83d61a03f0feb4a01d9ea0589
Alberto Bietti <alberto@bietti.me>
2021-05-14 15:53:11 -0400

    .

--- main_ebm.py
+++ main_ebm.py
@@ -22,8 +22,8 @@ class Net(nn.Module):
         self.features = nn.Sequential(
                 nn.Conv2d(3, 16 * width, kernel_size=3, stride=1, padding=0, bias=False),
                 nn.ReLU(inplace=True),
-                nn.MaxPool2d(kernel_size=2, stride=2),
-                nn.Conv2d(16 * width, 32 * width, kernel_size=3, stride=1, padding=0, bias=False),
+                nn.AvgPool2d(kernel_size=16, stride=16),
+                # nn.Conv2d(16 * width, 32 * width, kernel_size=3, stride=1, padding=0, bias=False),
                 # nn.ReLU(inplace=True),
                 # nn.MaxPool2d(kernel_size=2, stride=2),
                 # nn.Conv2d(32 * width, 64 * width, kernel_size=3, stride=2, padding=0, bias=False),
@@ -64,8 +64,8 @@ if __name__ == '__main__':
     parser.add_argument('--niter', default=501, type=int)
     parser.add_argument('--eval_delta', default=20, type=int,
                         help='evaluate every delta epochs')
-    parser.add_argument('--lr', default=0.01, type=float)
-    parser.add_argument('--beta', default=0.01, type=float)
+    parser.add_argument('--lr', default=0.1, type=float)
+    parser.add_argument('--beta', default=100., type=float)
     parser.add_argument('--batch_size', default=128, type=int)
     parser.add_argument('--test_batch_size', default=256, type=int)
     parser.add_argument('--aug', action='store_true', help='data augmentation')
@@ -122,7 +122,7 @@ if __name__ == '__main__':
         sz = min(args.nsamples - idx, ims.shape[0])
         X[idx:idx+sz] = ims[:sz]
 
-    X += 0.000001 * torch.randn(X.shape)
+    X += 0.00001 * torch.randn(X.shape)
     X.requires_grad_()
 
     # for saving some images
@@ -132,11 +132,6 @@ if __name__ == '__main__':
     mean_map = torch.zeros(mean_map_tr.shape, device=device)
     for it in range(args.niter):
         logging.info(f'iteration {it}')
-        if it % args.eval_delta == 0:
-            # plot samples
-            grid = torchvision.utils.make_grid(X[:100])
-            writer.add_image('samples', grid, it)
-            # writer.close()
 
         # compute mean embedding of current walkers
         for i in range((args.nsamples - 1) // args.batch_size + 1):
@@ -151,6 +146,14 @@ if __name__ == '__main__':
 
         mmd = (mean_map - mean_map_tr).norm()
 
+        # evaluate
+        if it % args.eval_delta == 0:
+            logging.info(f'mmd = {mmd}')
+            # plot samples
+            grid = torchvision.utils.make_grid(X[:100])
+            writer.add_image('samples', grid, it)
+            # writer.close()
+
         # gradient updates for walkers
         if X.grad is not None:
             X.grad.zero_()
@@ -162,5 +165,5 @@ if __name__ == '__main__':
             Usum.backward()
 
         X.data.sub_(args.lr * X.grad
-                + torch.sqrt(2 * args.lr * mmd / args.beta) * torch.randn(X.shape))
+                + torch.sqrt(args.lr * mmd / args.beta) * torch.randn(X.shape))
 
